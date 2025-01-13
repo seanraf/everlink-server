c3394f42351c7801d811817254e6ef8b35d9b788
Hammad <hammad391200@gmail.com>
2025-01-13 16:24:39 +0500

    `Update MongoDB connection URI in app.js`

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
